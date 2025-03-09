2a77f606ac8db0e7394a9724be91f58cded9e0ab
Tomas Ruiz <tomas.ruiz.te@gmail.com>
2025-03-09 04:51:49 +0100

    Improve individual activation patching visualization

--- st_viz_act_patchin.py
+++ st_viz_act_patchin.py
@@ -26,13 +26,25 @@ dataset, img_ids = get_dataset_cached()
 root = Path("vqa_patching")
 
 # folder1 = "str/*"
-folder1 = "gn_sigma_xs"
+folder1 = "str"
 cases1: list[str] = sorted(f.name for f in root.glob(f"{folder1}/*"))
 st.write(f"num {folder1} cases:", len(cases1))
-folder2 = "gn"
+folder2 = "gn_03o"
 cases2: list[str] = sorted(f.name for f in root.glob(f"{folder2}/*"))
 st.write(f"num {folder2} cases:", len(cases2))
-joint_cases = sorted(set(cases1) & set(cases2))
+folder3 = "gn_10o"
+cases3: list[str] = sorted(f.name for f in root.glob(f"{folder3}/*"))
+st.write(f"num {folder3} cases:", len(cases3))
+folder4 = "gn_20o"
+cases4 = sorted(f.name for f in root.glob(f"{folder4}/*"))
+st.write(f"num {folder4} cases:", len(cases4))
+folder5 = "gn_30o"
+cases5 = sorted(f.name for f in root.glob(f"{folder5}/*"))
+st.write(f"num {folder5} cases:", len(cases5))
+
+joint_cases = sorted(
+    set(cases1) & set(cases2) & set(cases3) & set(cases4) & set(cases5)
+)
 st.write("num joint cases:", len(joint_cases))
 
 if len(joint_cases) == 0:
@@ -44,7 +56,9 @@ case = st.radio("Select a case", joint_cases, horizontal=True)
 
 pr1 = ActivationPatchingResult.load(root / folder1 / case)
 pr2 = ActivationPatchingResult.load(root / folder2 / case)
-
+pr3 = ActivationPatchingResult.load(root / folder3 / case)
+pr4 = ActivationPatchingResult.load(root / folder4 / case)
+pr5 = ActivationPatchingResult.load(root / folder5 / case)
 c1, c2 = st.columns(2)
 
 with c1:
@@ -107,7 +121,38 @@ c1, c2 = st.columns(2)
 
 logits_diff1 = pr1.get_logit_diff_str() if "str" in folder1 else pr1.get_logit_diff_gn()
 logits_diff2 = pr2.get_logit_diff_str() if "str" in folder2 else pr2.get_logit_diff_gn()
+logits_diff3 = pr3.get_logit_diff_str() if "str" in folder3 else pr3.get_logit_diff_gn()
+logits_diff4 = pr4.get_logit_diff_str() if "str" in folder4 else pr4.get_logit_diff_gn()
+logits_diff5 = pr5.get_logit_diff_str() if "str" in folder5 else pr5.get_logit_diff_gn()
+# denominators
+denom1 = (
+    pr1.logit_diff_denominator_str()
+    if "str" in folder1
+    else pr1.logit_diff_denominator_gn()
+)
+denom2 = (
+    pr2.logit_diff_denominator_str()
+    if "str" in folder2
+    else pr2.logit_diff_denominator_gn()
+)
+denom3 = (
+    pr3.logit_diff_denominator_str()
+    if "str" in folder3
+    else pr3.logit_diff_denominator_gn()
+)
+denom4 = (
+    pr4.logit_diff_denominator_str()
+    if "str" in folder4
+    else pr4.logit_diff_denominator_gn()
+)
+denom5 = (
+    pr5.logit_diff_denominator_str()
+    if "str" in folder5
+    else pr5.logit_diff_denominator_gn()
+)
 
+with c1:
+    st.write("**denominators:**", denom1, denom2, denom3, denom4, denom5)
 
 with c1:
     st.write(f"### Reduction: mean, Folder: {folder1}")
@@ -115,6 +160,15 @@ with c1:
     st.write(fig)
     st.write(f"### Reduction: mean, Folder: {folder2}")
     fig = plot_img_and_text_probs_side_by_side(logits_diff2, reduction="mean", **kwargs)
+    st.write(fig)
+    st.write(f"### Reduction: mean, Folder: {folder3}")
+    fig = plot_img_and_text_probs_side_by_side(logits_diff3, reduction="mean", **kwargs)
+    st.write(fig)
+    st.write(f"### Reduction: mean, Folder: {folder4}")
+    fig = plot_img_and_text_probs_side_by_side(logits_diff4, reduction="mean", **kwargs)
+    st.write(fig)
+    st.write(f"### Reduction: mean, Folder: {folder5}")
+    fig = plot_img_and_text_probs_side_by_side(logits_diff5, reduction="mean", **kwargs)
     st.write(fig)
 
 with c2:
@@ -126,5 +180,20 @@ with c2:
     st.write(f"### Reduction: absmax, Folder: {folder2}")
     fig = plot_img_and_text_probs_side_by_side(
         logits_diff2, **kwargs, reduction="absmax"
+    )
+    st.write(fig)
+    st.write(f"### Reduction: absmax, Folder: {folder3}")
+    fig = plot_img_and_text_probs_side_by_side(
+        logits_diff3, **kwargs, reduction="absmax"
+    )
+    st.write(fig)
+    st.write(f"### Reduction: absmax, Folder: {folder4}")
+    fig = plot_img_and_text_probs_side_by_side(
+        logits_diff4, **kwargs, reduction="absmax"
+    )
+    st.write(fig)
+    st.write(f"### Reduction: absmax, Folder: {folder5}")
+    fig = plot_img_and_text_probs_side_by_side(
+        logits_diff5, **kwargs, reduction="absmax"
     )
     st.write(fig)
